9eeedf542218295d1fa11f8d16254d082c7c1ae7
Helena Wu <Helena.wu87@gmail.com>
2015-03-05 15:20:17 -0500

    Configured app for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,10 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  #https://helena-bloccit-2015.herokuapp.com
+  config.action_mailer.default_url_options = {host: 'https://helena-bloccit-2015.herokuapp.com'}
+  config.force_ssl = true
+  
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
