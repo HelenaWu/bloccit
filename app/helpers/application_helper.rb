a9ba9902452ed630ce1da8399e715fa652dd3780
Helena Wu <Helena.wu87@gmail.com>
2015-03-20 10:13:19 -0400

    Completed paginate assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  @@per_page_max = 0
+
   def form_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'form-group has-error'
@@ -15,7 +17,28 @@ module ApplicationHelper
   end
 
   def will_paginate(results)
-    next_page = (params[:page] || 0).to_i + 1
-    link_to "Next", "?page=#{next_page}"
+    # next_page = (params[:page] || 0).to_i + 1
+    # link_to "Next", "?page=#{next_page}"
+    result = ""
+    if results.length > @@per_page_max
+      @@per_page_max = results.length
+    end
+    curr_page = (params[:page] || 0).to_i
+    next_page = curr_page + 1
+
+    if results.length >= @@per_page_max 
+      result = "#{link_to 'Next', "?page=#{next_page}"}"  
+      if curr_page > 0
+        result += "|"
+      end
+    end
+
+    if curr_page > 0
+      prev_page = curr_page - 1
+      result = result + " #{link_to "Prev", "?page=#{prev_page}"}"
+    end   
+    return result.html_safe
+     
   end
+
 end
