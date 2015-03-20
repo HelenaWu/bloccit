264c8ff9566505816baf76e6c0fd952d1d51201f
Helena Wu <Helena.wu87@gmail.com>
2015-03-19 21:46:23 -0400

    fixed comment error display issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,8 @@ class CommentsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:post_id])
+    @comments = @post.comments
+    
     # for some reason comment_params was not recognized
     @comment = Comment.new(body: params[:comment][:body])
     @comment.post = @post
@@ -20,7 +22,8 @@ class CommentsController < ApplicationController
       redirect_to topic_post_url(@topic, @post)
     else      
       flash[:error] = "There was an error saving the comment. Please try again."
-      redirect_to topic_post_url(@topic, @post)
+      # redirect_to topic_post_url(@topic, @post)
+      render "posts/show"
     end
   end
 end
