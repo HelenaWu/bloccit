aeb626e9572b114258d4ba515b31a226c9199f4b
Helena Wu <Helena.wu87@gmail.com>
2015-03-22 10:57:36 -0400

    added missing vote_spec.rb

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vote do
+  before do
+    @vote = Vote.create(value: 2)    
+  end
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect(@vote.valid?).to eq(false)
+      end
+    end
+  end
 end
