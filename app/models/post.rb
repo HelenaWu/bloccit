a8dce369855aae999f6e20a6cefc177580e89f3b
Helena Wu <Helena.wu87@gmail.com>
2015-03-22 11:21:29 -0400

    Completed voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,7 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   # validates :topic, presence: true
   # validates :user, presence: true
+  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -30,5 +31,10 @@ class Post < ActiveRecord::Base
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
   end
-  
+
+
+  private
+  def create_vote
+    user.votes.create(value: 1, post: self, user: user)
+  end
 end
