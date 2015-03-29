6df1f44d8c3f270a7ab69fd5433cd610266b1fcc
Helena Wu <Helena.wu87@gmail.com>
2015-03-29 13:03:03 -0400

    completed Interlude 2 checkpoint

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -10,10 +10,14 @@ class Comment < ActiveRecord::Base
   private
   def send_favorite_emails
     post.favorites.each do |favorite|
-      #Should I place a check here? Or should I have stubbed this out for rspec?
-      if favorite.user
-        FavoriteMailer.new_comment(favorite.user, post, self).deliver
+      if favorite.user && should_receive_update_for?(favorite) 
+        FavoriteMailer.new_comment(favorite.user, post, self).deliver_now
       end
-    end
+    end  
   end
+
+  def should_receive_update_for?(favorite)
+    favorite.user_id != self.user_id && favorite.user.email_favorites?
+  end
+
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,7 @@ describe Comment do
   include TestFactories
 
   describe "after_create" do
+   
     before do
       @post = associated_post
       @user = authenticated_user
@@ -11,22 +12,36 @@ describe Comment do
      
     end
 
-    it "sends an email to users who have favourited the post" do
-      @user.favorites.where(post: @post).create
+    context "with user's permission" do
+      it "sends an email to users who have favourited the post" do
+        @user.favorites.where(post: @post).create
+        
+        allow(FavoriteMailer)
+          .to receive(:new_comment)
+          .with(@user, @post, @comment)
+          .and_return(double(deliver_now: true))
+
+        @comment.save
+      end
+      it "does not send emails to users who haven't" do
+        expect(FavoriteMailer)
+          .not_to receive(:new_comment)
+
+        @comment.save
+      end
+   end  
+
+    context "without permission" do
+
+      before { @user.update_attribute(:email_favorites, false)}
       
-      allow(FavoriteMailer)
-        .to receive(:new_comment)
-        .with(@user, @post, @comment)
-        .and_return(double(deliver_now: true))
+      it "does not send emails, even to users who have favorited" do
+        @user.favorites.where(post: @post).create
+        expect(FavoriteMailer)
+          .not_to receive(:new_comment)
 
-      @comment.save
-    end
-
-    it "does not send emails to users who haven't" do
-      expect(FavoriteMailer)
-        .not_to receive(:new_comment)
-
-      @comment.save
+        @comment.save
+      end
     end
   end
 end
