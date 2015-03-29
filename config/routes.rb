29ec9fcd832eb2d818cd5b4ecb39be780e22fe77
Helena Wu <Helena.wu87@gmail.com>
2015-03-29 19:01:41 -0400

    Completed popular posts checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
   resources :questions
 
   resources :topics do 
-    resources :posts, except: [:index] do
-    end
+    resources :posts, except: [:index], controller: 'topics/posts'
   end
-  resources :posts, only: [] do
+  resources :posts, only: [:index] do
     resources :comments, only: [ :create, :destroy]
-      resources :favorites, only: [:create, :destroy]
-      post '/up-vote' => 'votes#up_vote', as: :up_vote 
-      post '/down-vote' => 'votes#down_vote', as: :down_vote
+    resources :favorites, only: [:create, :destroy]
+    post '/up-vote' => 'votes#up_vote', as: :up_vote 
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   get 'about' => 'welcome#about'
 
