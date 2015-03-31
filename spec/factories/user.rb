c7be1944d89e811e444af86a651cc436921180b4
Helena Wu <Helena.wu87@gmail.com>
2015-03-30 20:31:00 -0400

    Completed testing users assignment

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,20 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     confirmed_at Time.now
+
+    factory :user_with_post_and_comment do
+      transient do
+        num_comments 1
+      end
+
+      after(:build) do |user, evaluator|
+        post = create(:post, user: user)
+        evaluator.num_comments.times  {
+          comment = create(:comment, user: user, post: post)
+        }
+      end
+    end
   end
+
+  
 end
